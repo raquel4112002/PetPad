c2c80cbc33e54c7c5b0c3ee49cd7a6405b2ccfe0
raquel4112002 <68569885+raquel4112002@users.noreply.github.com>
2023-01-10 16:07:37 +0000

    comentarios disign

--- app/src/main/java/ipca/utility/petpad/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/ipca/utility/petpad/ui/Profile/ProfileFragment.kt
@@ -40,7 +40,7 @@ class ProfileFragment : Fragment() {
             val currentUser = FirebaseAuth.getInstance().currentUser
             db.collection("users")
                 .document(currentUser?.uid!!)
-                .set(hashMapOf("name" to binding.etFirstName.text.toString(),
+                .set(hashMapOf("name" to binding.etFirstName.text.toString(),"last name" to binding.etLastName.text.toString(),
                 "bio" to binding.etBio.text.toString()))
         }
 
